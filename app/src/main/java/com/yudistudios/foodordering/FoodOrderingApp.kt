d96b1abca1f10dd9db541ddaad3f6c4e85cf8641
Yunus Dilber <yunusdilber99@gmail.com>
2022-03-06 22:23:33 +0300

    initial

---
+++ app/src/main/java/com/yudistudios/foodordering/FoodOrderingApp.kt
@@ -0,0 +1,15 @@
+package com.yudistudios.foodordering
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class FoodOrderingApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+     }
+}
